4deb26e582c07951da5b01f712c14444e3d96891
love patidar <engrlovepatidar@gmail.com>
2026-08-06 18:47:19 +0530

    Add Lunatic Never Content [B] from Codeforces

---
+++ 1826/B - Lunatic Never Content/solution.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <numeric>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int,int>;
+ 
+#define all(x) (x).begin(), (x).end()
+#define endl '
+'
+void solve() {
+    int n ; 
+    cin >> n ; 
+    vector<int> a(n) ; 
+    for(int i = 0 ; i< n;  i++) cin >> a[i] ; 
+ 
+    int ans = 0 ;
+    bool check = true; 
+    for(int i = 0 ; i< n /2 ; i++){
+        if(a[i] != a[n-i-1]) {
+            check = false ; 
+            break ; 
+        }
+    }
+    if(check){
+        cout << 0 << endl ; 
+        return ; 
+    }
+ 
+    for(int i = 0 ; i < n/2 ; i++){
+        ans = __gcd(ans ,abs(a[i] -a[n-i-1])) ; 
+    }
+    cout << ans <<endl; 
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--)
+        solve();
+ 
+    return 0;
+}
